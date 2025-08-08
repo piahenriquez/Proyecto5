50ad6ab486d510a0f1f1108679bb285bd6d85788
unknown <pihenriquez.dwfs19@bootcampudd.cl>
2025-08-08 17:37:37 -0400

    mejora de diseños header y footer

---
+++ src/pages/About/About.jsx
@@ -0,0 +1,9 @@
+
+
+const About = () => {
+  return (
+    <h1>Sobre nosotros</h1>
+  )
+}
+
+export default About
---
+++ src/pages/Hooks/Hooks.jsx
@@ -0,0 +1,8 @@
+
+const Hooks = () => {
+  return (
+    <div>Hooks</div>
+  )
+}
+
+export default Hooks
