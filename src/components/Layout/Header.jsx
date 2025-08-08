b7d5233aa8a23a1685c34c0db8cdb83202ab543b
unknown <pihenriquez.dwfs19@bootcampudd.cl>
2025-08-07 22:32:13 -0400

    Proyecto base limpio

---
+++ src/components/Layout/Header.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom"
+
+const Header = () => {
+  return (
+    <header >
+      <h1>Aplicación</h1>
+      <nav>
+        <Link to="/">Inicio</Link>
+        <Link to="/about">Acerca de</Link>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
